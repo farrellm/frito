a52fc81179ca3975d0f657426ad2621c945ec5da
Matthew Farrel <farrellm@alum.mit.edu>
2014-03-07 20:54:16 -0500

    initial

---
+++ project.clj
@@ -0,0 +1,11 @@
+(defproject frito "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.clojure/core.async "0.1.267.0-0d7780-alpha"]
+                 [org.clojure/core.match "0.2.1"]
+                 [clj-http "0.9.0"]
+                 [enlive "1.1.5"]
+                 [swiss-arrows "1.0.0"]])
---
+++ test/frito/core_test.clj
@@ -0,0 +1,7 @@
+(ns frito.core-test
+  (:require [clojure.test :refer :all]
+            [frito.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
